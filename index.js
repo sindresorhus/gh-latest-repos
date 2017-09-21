c8bf09c0c2e39762ec9e1339fb076e7cac3c0ab5
Sindre Sorhus <sindresorhus@gmail.com>
2017-09-22 02:05:07 +0700

    Init

---
+++ index.js
@@ -0,0 +1,74 @@
+'use strict';
+const got = require('got');
+
+const token = process.env.GITHUB_TOKEN;
+const username = process.env.GITHUB_USERNAME;
+const origin = process.env.ORIGIN;
+const ONE_DAY = 1000 * 60 * 60 * 24;
+
+if (!token) {
+	throw new Error('Please set your GitHub token in the `GITHUB_TOKEN` environment variable');
+}
+
+if (!username) {
+	throw new Error('Please set your GitHub username in the `GITHUB_USERNAME` environment variable');
+}
+
+if (!origin) {
+	throw new Error('Please set the `access-control-allow-origin` you want in the `ORIGIN` environment variable');
+}
+
+const query = `
+	query {
+		user(login: "${username}") {
+			repositories(
+				last: 6,
+				isFork: false,
+				affiliations: OWNER,
+				privacy: PUBLIC,
+				orderBy: {
+					field: CREATED_AT,
+					direction: ASC
+				}
+			) {
+				nodes {
+					name
+					description
+					url
+					primaryLanguage {
+						name
+						color
+					}
+				}
+			}
+		}
+	}
+`;
+
+let responseText = '[]';
+
+async function fetchRepos() {
+	const {body} = await got.post('https://api.github.com/graphql', {
+		json: true,
+		body: {query},
+		headers: {
+			authorization: `bearer ${token}`
+		}
+	});
+
+	const repos = body.data.user.repositories.nodes;
+	responseText = JSON.stringify(repos);
+}
+
+setInterval(fetchRepos, ONE_DAY);
+fetchRepos();
+
+module.exports = (request, response) => {
+	response.setHeader('access-control-allow-origin', origin);
+
+	if (origin !== '*') {
+		response.setHeader('vary', 'origin');
+	}
+
+	response.end(responseText);
+};
---
+++ readme.md
@@ -0,0 +1,40 @@
+# gh-latest-repos
+
+> Microservice to get the latest public GitHub repos from a user
+
+I currently use this on [my website](https://sindresorhus.com/#projects).
+
+It returns the 6 latest repos and it's cached for a day.
+
+[Example response](example-response.json)
+
+
+## Usage
+
+### With [`now`](https://now.sh)
+
+[![Deploy to now](https://deploy.now.sh/static/button.svg)](https://deploy.now.sh/?repo=https://github.com/sindresorhus/gh-latest-repos&env=GITHUB_TOKEN&env=GITHUB_USERNAME&env=ORIGIN)
+
+or
+
+```
+$ now sindresorhus/gh-latest-repos -e NODE_ENV=production -e GITHUB_TOKEN=xxx -e GITHUB_USERNAME=xxx -e ORIGIN=xxx
+```
+
+### Manual
+
+Deploy to your hosting provider, set the below environment variables, and start it with `npm start`.
+
+
+## Environment variables
+
+Define the following environment variables:
+
+- `GITHUB_TOKEN` - [Personal access token.](https://github.com/settings/tokens/new?description=gh-latest-repos)
+- `GITHUB_USERNAME` - The username you like to get repos from.
+- `ORIGIN` - The URL of your website or `*` if you want to allow any origin (not recommended), for the `Access-Control-Allow-Origin` header.
+
+
+## License
+
+MIT © [Sindre Sorhus](https://sindresorhus.com)
